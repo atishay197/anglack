af1fe68fa033947ab412d2c147a1640bb49b09ac
atishay197 <atishay197@gmail.com>
2016-04-23 21:09:10 +0530

    Adding recording...

--- ImageSpeech/facedetect.py
+++ ImageSpeech/facedetect.py
@@ -3,41 +3,48 @@ from PIL import Image
 
 hodClient = HODClient("a1d1963c-6c3e-487c-b339-224747ac81b2", "v1")
 
-hodApp = HODApps.DETECT_FACES
-paramArr = {}
-paramArr["file"] = "testdata/us1.jpg"
-paramArr["additional"] = "True"
-im=Image.open(paramArr["file"])
-im.size
-print im.size[0]
 
-response = hodClient.post_request(paramArr, hodApp, async=False)
+def facedetect():
+	hodApp = HODApps.DETECT_FACES
+	paramArr = {}
+	paramArr["file"] = "testdata/us1.jpg"
+	paramArr["additional"] = "True"
+	im=Image.open(paramArr["file"])
+	im.size
+	print im.size[0]
 
-if response is None:
-	error = hodClient.get_last_error();
-	for err in error.errors:
-		print ("Error code: %d \nReason: %s \nDetails: %s\n" % (err.error,err.reason, err.detail))
-else:
-	x = response['face']
-	NoOfPeople = len(x)
-	statement = "Total of " + str(len(x)) + " people.\n"
-	for person in x:
-		print person
-		position = " "
-		try:
-			personage =  person['additional_information']
-			age = personage['age']
-		except KeyError:
-			age = "person"
-		if(person['left'] < im.size[0]/2):
-			position = "left"
-		else:
-			position = "right"
-		statement += age + " on your " + position + ".\n"
-	print statement
+	response = hodClient.post_request(paramArr, hodApp, async=False)
+
 	if response is None:
 		error = hodClient.get_last_error();
 		for err in error.errors:
-			print ("Error code: %d \nReason: %s \nDetails: %s\njobID: %s\n" % (err.error, err.reason, err.detail, err.jobID))
+			print ("Error code: %d \nReason: %s \nDetails: %s\n" % (err.error,err.reason, err.detail))
+	else:
+		x = response['face']
+		NoOfPeople = len(x)
+		if(NoOfPeople == 0):
+			statement = "No one in the vicinity\n"
+		else:
+			statement = "Total of " + str(len(x)) + " people.\n"
+			for person in x:
+				print person
+				position = " "
+				try:
+					personage =  person['additional_information']
+					age = personage['age']
+				except KeyError:
+					age = "person"
+				if(person['left'] < im.size[0]/2):
+					position = "left"
+				else:
+					position = "right"
+				statement += age + " on your " + position + ".\n"
+			# print statement
+			if response is None:
+				error = hodClient.get_last_error();
+				for err in error.errors:
+					print ("Error code: %d \nReason: %s \nDetails: %s\njobID: %s\n" % (err.error, err.reason, err.detail, err.jobID))
 
-#return statement
+	return statement
+
+print facedetect()
